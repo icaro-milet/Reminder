df56ad147a505b10bdebbde1d911073da00b8e83
icaro-milet <milet.icaro@gmail.com>
2022-03-27 20:30:01 -0300

    Create button

--- source/Reminder/ViewController.swift
+++ source/Reminder/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var create: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func createReminder(_ sender: Any) {
+        
+    }
+    
 }
 
