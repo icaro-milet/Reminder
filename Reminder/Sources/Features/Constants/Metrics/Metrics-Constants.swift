ee1082165d2e6a784a7706ffc34e752d4b1bf21e
milet <milet@milets-Mac-mini.local>
2025-06-09 19:58:18 -0300

    feat: first commit

---
+++ Reminder/Sources/Features/Constants/Metrics/Metrics-Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Metrics-Constants.swift
+//  Reminder
+//
+//  Created by milet on 09/06/25.
+//
+
+import Foundation
+import UIKit
+
+public struct Metrics {
+    
+    public static var huge: CGFloat = 48
+    public static var medium: CGFloat = 24
+    public static var small: CGFloat = 12
+    public static var tiny: CGFloat = 8
+    public static var little: CGFloat = 4
+}
