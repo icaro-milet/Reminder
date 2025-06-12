e07246674b780dd7e542378ec543c935c7a40945
milet <milet@milets-Mac-mini.local>
2025-06-12 20:56:13 -0300

    feat: adding login

--- Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -6,10 +6,18 @@
 //
 
 import Foundation
+import Firebase
 
 class LoginBottomSheetViewModel {
     
     func doAuth(usernameLogin: String, password: String) {
         print(usernameLogin)
+        Auth.auth().signIn(withEmail: usernameLogin, password: password) { [weak self] authResult, error in
+            if let error = error {
+                print("autenticação falhou \(error)")
+            } else {
+                print("Sucesso", authResult)
+            }
+        }
     }
 }
