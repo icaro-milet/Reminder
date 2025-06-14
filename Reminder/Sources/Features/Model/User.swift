2ca522f53d4654bb38f84e7b8cabe7c46169f071
milet <milet@milets-Mac-mini.local>
2025-06-14 16:42:59 -0300

    feat: adding User Defaults

---
+++ Reminder/Sources/Features/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Reminder
+//
+//  Created by milet on 14/06/25.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    let email: String
+    let isUserSaved: Bool
+}
---
+++ Reminder/Sources/Features/UserDefaults/UserDefaultsManager.swift
@@ -0,0 +1,34 @@
+//
+//  UserDefaultsManager.swift
+//  Reminder
+//
+//  Created by milet on 14/06/25.
+//
+
+import Foundation
+
+class UserDefaultsManager {
+    
+    private static let userKey = "userKey"
+    
+    static func saveUser(user: User) {
+        let encoder = JSONEncoder()
+        
+        if let encoded = try? encoder.encode(user) {
+            UserDefaults.standard.set(encoded, forKey: userKey)
+            UserDefaults.standard.synchronize()
+        }
+    }
+    
+    static func loadUser() -> User? {
+        if let userData = UserDefaults.standard.data(forKey: userKey) {
+            let decoder = JSONDecoder()
+            
+            if let user = try? decoder.decode(User.self, from: userData) {
+                return user
+            }
+        }
+        
+        return nil
+    }
+}
