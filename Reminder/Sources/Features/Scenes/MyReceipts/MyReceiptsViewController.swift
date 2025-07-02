47c9f2df13882721ef61ba4031e8a295f652c5be
milet <milet@milets-Mac-mini.local>
2025-07-02 14:25:19 -0300

    feat: table view with mocked data

--- Reminder/Sources/Features/Scenes/MyReceipts/MyReceiptsViewController.swift
+++ Reminder/Sources/Features/Scenes/MyReceipts/MyReceiptsViewController.swift
@@ -12,6 +12,13 @@ class MyReceiptsViewController: UIViewController {
     let contentView: MyReceiptsView
     weak var flowDelegate: MyReceiptsFlowDelegate?
     
+    private let mockMedicamentos = [
+        ("Buscopam", "14:00", "2 em 2 horas"),
+        ("Dipirona", "15:30", "8 em 8 horas"),
+        ("Velafaxina", "8:00", "Um por dia"),
+        ("Alegra", "9:45", "2 em 2 horas")
+    ]
+    
     init(contentView: MyReceiptsView,
          flowDelegate: MyReceiptsFlowDelegate) {
         self.contentView = contentView
@@ -26,6 +33,7 @@ class MyReceiptsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        setupTableView()
     }
     
     private func setup() {
@@ -45,4 +53,48 @@ class MyReceiptsViewController: UIViewController {
             contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
+    
+    private func setupTableView() {
+        contentView.tableView.dataSource = self
+        contentView.tableView.delegate = self
+        contentView.tableView.register(RemedyCell.self, forCellReuseIdentifier: RemedyCell.identifier)
+        contentView.tableView.contentInset = UIEdgeInsets(top: 16, left: 0, bottom: 16, right: 0)
+    }
+}
+
+extension MyReceiptsViewController: UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return mockMedicamentos.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = .clear
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 4
+    }
+}
+
+extension MyReceiptsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: RemedyCell.identifier, for: indexPath) as! RemedyCell
+        let medicamento = mockMedicamentos[indexPath.section]
+        cell.configure(title: medicamento.0, time: medicamento.1, recurrence: medicamento.2)
+        
+        return cell
+    }
+    
+    
 }
